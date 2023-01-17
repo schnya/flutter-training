d4836b709118e53c1ac5768b5a511375e77b8d4e
schnya <kda7sShump.aur@gmail.com>
2023-01-17 09:09:29 +0900

    Fix #5

---
+++ lib/src/page/components/mixin/push_home_page_mixin.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:training/src/page/home_page.dart';
+
+mixin PushHomePageMixin on State {
+  final duration = Duration(milliseconds: 500);
+  final schedulerBinding = SchedulerBinding.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    loop();
+  }
+
+  FutureOr<void> loop([dynamic _]) {
+    // Routeは毎度新しくないといけない。多分disposeしてるから。
+    return Future.delayed(
+      duration,
+      () => schedulerBinding.endOfFrame
+          .then((_) => Navigator.of(context).push(MyHomePage.route())),
+    ).then<void>(loop);
+  }
+}
--- lib/src/page/new_green_page.dart
+++ lib/src/page/new_green_page.dart
@@ -1,33 +1,12 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:training/src/page/home_page.dart';
+import 'components/mixin/push_home_page_mixin.dart';
 
 class NewGreenPage extends StatefulWidget {
   @override
-  State<NewGreenPage> createState() => _NewGreenPageState();
+  State createState() => _NewGreenPageState();
 }
 
-class _NewGreenPageState extends State<NewGreenPage> {
-  final duration = Duration(milliseconds: 500);
-  final schedulerBinding = SchedulerBinding.instance;
-
-  @override
-  void initState() {
-    super.initState();
-    loop();
-  }
-
-  FutureOr<void> loop([dynamic _]) {
-    // Routeは毎度新しくないといけない。多分disposeしてるから。
-    return Future.delayed(
-      duration,
-      () => schedulerBinding.endOfFrame
-          .then((_) => Navigator.of(context).push(MyHomePage.route())),
-    ).then<void>(loop);
-  }
-
+class _NewGreenPageState extends State with PushHomePageMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(backgroundColor: Colors.green);
